c1b8711b1d4b4f5f65af374ff85d83a21a63a34b
alex <alex@ibbd.net>
2015-11-19 23:54:36 +0800

    install

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ else
     echo '~/.vimrc文件已经存在'
 fi
 
+# 配置插件目录
 if [ ! -d ~/.vim/bundle ]; then
     mkdir ~/.vim/bundle -p
 fi
@@ -18,6 +19,7 @@ else
     git clone http://github.com/gmarik/vundle.git
 fi 
 
-
+# 安装或者更新
 vim +BundleInstall! +BundleClean +q
 
+echo 'All is ok!'
