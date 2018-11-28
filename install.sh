7c084d9d97b1689eb591b9e332347edc58b451b7
alex cai <cyy0523xc@gmail.com>
2018-11-28 11:42:55 +0800

    install

--- install.sh
+++ install.sh
@@ -36,6 +36,7 @@ if [ ! -f ~/.vim/colors/molokai.vim ]; then
     cp ./molokai/colors/molokai.vim ~/.vim/colors/
 fi
 
+"""
 # 交换大小写键和ESC键
 if which xmodmap; then
     if [ ! -f ~/.xmodmap ]; then 
@@ -46,6 +47,7 @@ if which xmodmap; then
 else 
     echo 'xmodmap还没有安装，无法交换大小写键和ESC键'
 fi
+"""
 
 # 配置插件目录
 if [ ! -d ~/.vim/bundle ]; then
