e0f7d1cb8b8b383cbe9c45fe8234324292684bb9
alex <cyy0523xc@gmail.com>
2020-03-31 15:03:02 +0800

    ycm to py3

--- install-YoucompleteMe.sh
+++ install-YoucompleteMe.sh
@@ -2,9 +2,9 @@
 # 
 # 安装YoucompleteMe需要额外的步骤
 # 
-sudo apt-get install cmake build-essential python-dev python3-dev
+# sudo apt-get install cmake build-essential python-dev python3-dev
 
 cd ~/.vim/bundle/YouCompleteMe
 git submodule update --init --recursive
-python install.py
+python3 install.py
 
